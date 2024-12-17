5a3f569b58e70b12790e6d893157a7cb6bb41a46
rafidreyes <164291682+rafidreyes@users.noreply.github.com>
2024-12-17 17:40:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { Client, logger } = require('./lib/client')
 const { DATABASE, VERSION } = require('./config')
 const { stopInstance } = require('./lib/pm2')
+
 const SESSION_ID = 'levanter_18cfab200b477e44c9840e653732520633'
+if (!SESSION_ID) {
+  console.error('SESSION_ID environment variable is not set.')
+  process.exit(1)
+}
 
 const start = async () => {
   logger.info(`levanter ${VERSION}`)
@@ -15,6 +20,11 @@ const start = async () => {
   try {
     const bot = new Client()
     await bot.connect()
+    if (ALWAYS_ONLINE) {
+      logger.info('Bot is set to always stay online.')
+    } else {
+      logger.info('Bot will not stay always online.')
+    }
   } catch (error) {
     logger.error(error)
   }
